b079bb0c2e0bb544ac26b11d9de20c8568e2b6a2
Stalemated <52725146+Stalemated@users.noreply.github.com>
2026-05-08 21:34:27 -0600

    Feat: creación del repositorio inicial y adición de application.yaml

---
+++ src/main/java/acalidonio/labo2pnc/Labo2PncApplication.java
@@ -0,0 +1,13 @@
+package acalidonio.labo2pnc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Labo2PncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Labo2PncApplication.class, args);
+    }
+
+}
---
+++ src/test/java/acalidonio/labo2pnc/Labo2PncApplicationTests.java
@@ -0,0 +1,13 @@
+package acalidonio.labo2pnc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Labo2PncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
